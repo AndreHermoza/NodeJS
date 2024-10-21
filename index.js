2d6c8eebda8deaf9ab979232aa4d8807b773e286
Andre Hermoza <andre.hermoza.gutierrez@vallegrande.edu.pe>
2024-10-21 02:41:28 -0500

    node js chatgpt

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 const PORT = 2008;
-const ip = '54.221.65.213'
+const ip = '54.221.65.213';
 
+app.use(express.static('public'));
 
-app.use(express.static('public'))
-
-
-app.get('/',(req,res) =>{
-    res,sendFile(path,join(__dirname, 'index.html'))
-} )
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));  // Asegúrate de que esta línea esté correcta
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://${ip}:${PORT}`)
-})
+    console.log(`Server running at http://${ip}:${PORT}`);
+});
